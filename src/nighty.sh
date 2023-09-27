fb69ce92c952a941390bb743a942c2929c039a73
jo-project <jo.project.0911@gmail.com>
2023-09-27 16:48:17 +1000

    Nighty

--- src/nighty.sh
+++ src/nighty.sh
@@ -58,7 +58,7 @@ main() {
     setw window-status-activity-style "underscore,fg=${thm_fgdark},bg=${thm_bg}"
     setw window-status-separator ""
     setw window-status-style "NONE,fg=${thm_fgdark},bg=${thm_bg}"
-    setw window-status-format "#[fg=${thm_bg},bg=${thm_bg},nobold,nounderscore,noitalics]#[default] #I  #W #F #[fg=${thm_bg},bg=${thm_bg},nobold,nounderscore,noitalics]"
+    setw window-status-format "#[fg=${thm_bg},bg=${thm_bg},nobold,nounderscore,noitalics]#[default] #I  #W #F #[fg=${thm_bgdark},bg=${thm_bg},nobold,nounderscore,noitalics]"
     setw window-status-current-format "#[fg=${thm_bg},bg=${thm_bg},nobold,nounderscore,noitalics]#[fg=${thm_blue},bg=${thm_bgdark},bold] #I  #W #F #[fg=${thm_bgdark},bg=${thm_bg},nobold,nounderscore,noitalics]"
 
     set status-left "#[fg=${thm_bgdark},bg=${thm_blue},bold] #S #[fg=${thm_blue},bg=${thm_bg},nobold,nounderscore,noitalics]"
