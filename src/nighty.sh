cc49b742d6dc75e67c45eba9a5cc9f3516e7d690
jo-project <jo.project.0911@gmail.com>
2023-09-27 17:12:01 +1000

    Nighty

--- src/nighty.sh
+++ src/nighty.sh
@@ -35,18 +35,18 @@ main() {
 
     source $current_dir/themes/nighty-$theme.tmuxtheme
 
-    set  mode-style "fg=#89b4fa,bg=#cba6f7"
+    set  mode-style "fg=#1e1e2e,bg=#cba6f7"
 
-    set  message-style "fg=#89b4fa,bg=#cba6f7"
-    set  message-command-style "fg=#89b4fa,bg=#cba6f7"
+    set  message-style "fg=#1e1e2e,bg=#cba6f7"
+    set  message-command-style "fg=#1e1e2e,bg=#cba6f7"
 
     set  pane-border-style "fg=#cba6f7"
-    set  pane-active-border-style "fg=#89b4fa"
+    set  pane-active-border-style "fg=#1e1e2e"
 
     set  status "on"
     set  status-justify "left"
 
-    set  status-style "fg=#89b4fa,bg=#181825"
+    set  status-style "fg=#1e1e2e,bg=#181825"
 
     set  status-left-length "100"
     set  status-right-length "100"
@@ -54,14 +54,14 @@ main() {
     set  status-left-style NONE
     set  status-right-style NONE
 
-    set  status-left "#[fg=#1E1E2E,bg=#89b4fa,bold] #S #[fg=#89b4fa,bg=#181825,nobold,nounderscore,noitalics]"
-    set  status-right "#[fg=#181825,bg=#181825,nobold,nounderscore,noitalics]#[fg=#89b4fa,bg=#181825] #{prefix_highlight} #[fg=#cba6f7,bg=#181825,nobold,nounderscore,noitalics]#[fg=#89b4fa,bg=#cba6f7] %Y-%m-%d  %I:%M %p #[fg=#89b4fa,bg=#cba6f7,nobold,nounderscore,noitalics]#[fg=#1E1E2E,bg=#89b4fa,bold]  #{b:pane_current_path} "
+    set  status-left "#[fg=#1E1E2E,bg=#89b4fa,bold] #S #[fg=#1e1e2e,bg=#181825,nobold,nounderscore,noitalics]"
+    set  status-right "#[fg=#181825,bg=#181825,nobold,nounderscore,noitalics]#[fg=#1e1e2e,bg=#181825] #{prefix_highlight} #[fg=#cba6f7,bg=#181825,nobold,nounderscore,noitalics]#[fg=#1e1e2e,bg=#cba6f7] %Y-%m-%d  %I:%M %p #[fg=#1e1e2e,bg=#cba6f7,nobold,nounderscore,noitalics]#[fg=#1E1E2E,bg=#89b4fa,bold]  #{b:pane_current_path} "
 
     setw  window-status-activity-style "NONE,fg=#cdd6f4,bg=#181825"
     setw  window-status-separator ""
     setw  window-status-style "NONE,fg=#cdd6f4,bg=#181825"
     setw  window-status-format "#[fg=#181825,bg=#181825,nobold,nounderscore,noitalics]#[default] #I  #W #F #[fg=#181825,bg=#181825,nobold,nounderscore,noitalics]"
-    setw  window-status-current-format "#[fg=#181825,bg=#cba6f7,nobold,nounderscore,noitalics]#[fg=#89b4fa,bg=#cba6f7,bold] #I  #W #F #[fg=#cba6f7,bg=#181825,nobold,nounderscore,noitalics]"
+    setw  window-status-current-format "#[fg=#181825,bg=#cba6f7,nobold,nounderscore,noitalics]#[fg=#1e1e2e,bg=#cba6f7,bold] #I  #W #F #[fg=#cba6f7,bg=#181825,nobold,nounderscore,noitalics]"
 
     # tmux-plugins/tmux-prefix-highlight support
     set  @prefix_highlight_output_prefix "#[fg=#ffc777]#[bg=#181825]#[fg=#181825]#[bg=#ffc777]"
