822b3b658531619101eba2e18881975b1d9cd7ea
jo-project <jo.project.0911@gmail.com>
2023-09-27 16:38:22 +1000

    Nighty

--- src/nighty.sh
+++ src/nighty.sh
@@ -39,16 +39,16 @@ main() {
     set status-justify "left"
     set status-left-length "100"
     set status-right-length "100"
-    set status-style "fg=${thm_blue},bg=${thm_bg}"
+    set status-style "fg=${thm_blue},bg=${thm_bgdark}"
     set status-left-style NONE
     set status-right-style NONE
 
     # Mode
-    set mode-style "fg=${thm_blue},bg=${thm_fg}"
+    set mode-style "fg=${thm_blue},bg=${thm_bgdark}"
 
     # Messages
-    set message-style "fg=${thm_blue},bg=${thm_fg}"
-    set message-command-style "fg=${thm_blue},bg=${thm_fg}"
+    set message-style "fg=${thm_blue},bg=${thm_bgdark}"
+    set message-command-style "fg=${thm_blue},bg=${thm_bgdark}"
 
     # Panes
     set pane-border-style "fg=${thm_fg}"
@@ -59,11 +59,11 @@ main() {
     setw window-status-separator ""
     setw window-status-style "NONE,fg=${thm_fgdark},bg=${thm_bg}"
     setw window-status-format "#[fg=${thm_bg},bg=${thm_bg},nobold,nounderscore,noitalics]#[default] #I  #W #F #[fg=${thm_bg},bg=${thm_bg},nobold,nounderscore,noitalics]"
-    setw window-status-current-format "#[fg=${thm_bg},bg=${thm_fg},nobold,nounderscore,noitalics]#[fg=${thm_blue},bg=${thm_fg},bold] #I  #W #F #[fg=${thm_fg},bg=${thm_bg},nobold,nounderscore,noitalics]"
+    setw window-status-current-format "#[fg=${thm_bg},bg=${thm_fg},nobold,nounderscore,noitalics]#[fg=${thm_blue},bg=${thm_bgdark},bold] #I  #W #F #[fg=${thm_bgdark},bg=${thm_bg},nobold,nounderscore,noitalics]"
 
     set status-left "#[fg=${thm_bgdark},bg=${thm_blue},bold] #S #[fg=${thm_blue},bg=${thm_bg},nobold,nounderscore,noitalics]"
 
-    set status-right "#[fg=${thm_bg},bg=${thm_bg},nobold,nounderscore,noitalics]#[fg=${thm_blue},bg=${thm_bg}] #{prefix_highlight} #[fg=${thm_fg},bg=${thm_bg},nobold,nounderscore,noitalics]#[fg=${thm_blue},bg=${thm_fg}] %Y-%m-%d  %I:%M %p #[fg=${thm_blue},bg=${thm_fg},nobold,nounderscore,noitalics]#[fg=${thm_bgdark},bg=${thm_blue},bold]  #{b:pane_current_path} "
+    set status-right "#[fg=${thm_bg},bg=${thm_bg},nobold,nounderscore,noitalics]#[fg=${thm_blue},bg=${thm_bg}] #{prefix_highlight} #[fg=${thm_bgdark},bg=${thm_bg},nobold,nounderscore,noitalics]#[fg=${thm_blue},bg=${thm_bgdark}] %Y-%m-%d  %I:%M %p #[fg=${thm_blue},bg=${thm_bgdark},nobold,nounderscore,noitalics]#[fg=${thm_bgdark},bg=${thm_blue},bold]  #{b:pane_current_path} "
 
     set @prefix_highlight_output_prefix "#[fg=${thm_yellow}]#[bg=${thm_bg}]#[fg=${thm_bg}]#[bg=${thm_yellow}]"
     set @prefix_highlight_output_suffix ""
